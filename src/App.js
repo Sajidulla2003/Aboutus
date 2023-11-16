e3736e59083bc96e3038abd3fce331f2b8edbbb5
Sajidulla2003 <shaiksajid11092003@gmail.com>
2023-11-16 17:53:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import {About} from './components/F2';
+import "./components/bootstrap-icons/bootstrap-icons.css";
+import "./components/boxicons/css/boxicons.min.css"
+import "./components/glightbox/css/glightbox.min.css"
+import "./components/remixicon/remixicon.css"
+import "./components/swiper/swiper-bundle.min.css"
+<link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet"></link>
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter>
+        
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
+
     </div>
   );
 }
---
+++ src/components/F2.js
@@ -0,0 +1,29 @@
+import "./style.css";
+export function About() {
+    return (
+        <div>
+            <header id="header">
+                <div class="container">
+
+                    <h1><i>Team 100</i></h1>
+                    <h2> <span>About Us</span> </h2>
+                    <h4><i></i></h4>
+
+                    <div class="social-links">
+                        <a href="https://www.instagram.com/" class="instagram"><i class="bi bi-instagram"></i></a>
+                        <a href="https://www.linkedin.com/in/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                        <a href="https://github.com/" class="github"><i class="bi bi-github"></i></a>
+                        <a href="1234567890" class="telephone"><i class="bi bi-telephone"></i></a>
+                    </div>
+
+                </div>
+            </header>
+        </div>
+        
+    )
+}
+
+
+
+
+
